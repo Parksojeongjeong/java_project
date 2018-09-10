24fa054050b4e212da790e87b47fe51f8b3c6046
rty52 <rty52@DESKTOP-FAMAUDC>
2018-09-10 20:14:51 +0900

    수정

--- java_project/src/java_project/Java_01.java
+++ java_project/src/java_project/Java_01.java
@@ -4,11 +4,23 @@ public class Java_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("프로젝트 주제: 나만의 웹 다이어리 만들기");
+		
 		System.out.println("일정 계획");
 		System.out.println("1) 멋사 2학기 활동 마무리");
 		System.out.println("2) 공강시간에 전공과목 예습 복습 하기");
 		System.out.println("3) 자바 플젝");
+		
+		System.out.println("상세계획: 주제: 나만의 웹 다이어리 만들기\r\n" + 
+				"\r\n" + 
+				"1) 계획 짜기\r\n" + 
+				"2) 사진 업로드하기\r\n" + 
+				"3) 수정 삭제 (CRUD 구현)\r\n" + 
+				"4) 프로필 만들기\r\n" + 
+				"( 부트스트랩 테마 이용, 체크박스를 이용해 하루 계획 달성 체크하기, 그래프 표현하기, 게시판 만들기 )\r\n" + 
+				"");
+		System.out.println("mysql, spring을 활용한 데이터 베이스 관리");
+		System.out.println("그래프 표현 공부하기");
+		
 		
 	}
 
