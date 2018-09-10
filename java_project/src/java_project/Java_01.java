65fe3165c80c995bc03931b7490062bf6d2acacd
rty52 <rty52@DESKTOP-FAMAUDC>
2018-09-10 11:16:59 +0900

    main

---
+++ java_project/src/java_project/Java_01.java
@@ -0,0 +1,15 @@
+package java_project;
+
+public class Java_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("프로젝트 주제: 나만의 웹 다이어리 만들기");
+		System.out.println("일정 계획");
+		System.out.println("1) 멋사 2학기 활동 마무리");
+		System.out.println("2) 공강시간에 전공과목 예습 복습 하기");
+		System.out.println("3) 자바 플젝");
+		
+	}
+
+}
